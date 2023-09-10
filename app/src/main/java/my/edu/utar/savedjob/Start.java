32ca75e39582e6a5e9b1321ce8b2cfeef287faf2
Kito <87749070+Kito1106@users.noreply.github.com>
2023-09-10 18:43:37 +0800

    Start.java

--- app/src/main/java/my/edu/utar/savedjob/Start.java
+++ app/src/main/java/my/edu/utar/savedjob/Start.java
@@ -17,7 +17,7 @@ public class Start extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
 
         new Handler().postDelayed(new Runnable() {
             @Override
